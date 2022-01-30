262628145a4b1736cc7b7d85676f36b136291ff2
Valtteri <github@vaarn.io>
2022-01-30 21:50:01 +0200

    segment masking testing

--- lcd.py
+++ lcd.py
@@ -28,29 +28,74 @@ def set_gpio_in(value:int, mask:int):
     IO_MUX_18[7] = 0  #pulldown disable
     IO_MUX_19[7] = 0
 
-async def runSegment():
-    print("seg task started")
 
-    Pin
+def initPins():
+    Pin(32, Pin.OUT).value(0)
+    Pin(33, Pin.OUT).value(0)
 
-    #initialize pins, otherwise set_gpio_out won't work
-    set_gpio_out(0b00000000000000000000000000000000,
-                 0b11111111111111111111111111111111)
+    Pin(12, Pin.OUT).value(0)
+    Pin(13, Pin.OUT).value(0)
+    Pin(14, Pin.OUT).value(0)
+    Pin(15, Pin.OUT).value(0)
+    Pin(16, Pin.OUT).value(0)
+    Pin(17, Pin.OUT).value(0)
+    Pin(18, Pin.OUT).value(0)
 
+    Pin(19, Pin.OUT).value(0)
+    Pin(21, Pin.OUT).value(0)
+    Pin(22, Pin.OUT).value(0)
+    Pin(23, Pin.OUT).value(0)
+    Pin(25, Pin.OUT).value(0)
+    Pin(26, Pin.OUT).value(0)
+    Pin(27, Pin.OUT).value(0)
+    
 async def runCommon():
     print("com task started")
+
+    initPins()
+    
+    seg1    = 0b11111111111111110111111111111111
+    seg2    = 0b11111111111111111111111111111111
+    enabled = 0b11111111111111111111111111111111
+    #mask =    0b11111111111111110111111111111111
+    mask    = 0b11111111111111110111111111111111 
 
     i = 0
     while(True):
         i += 1
-        Pin(18, Pin.OUT).value(i % 2)
-        Pin(19, Pin.IN)
+
+        enabled = ~ enabled
+        set_gpio_out(enabled, mask)
+
+        Pin(32, Pin.OUT).value(1)
+        Pin(33, Pin.IN)
         await uasyncio.sleep_ms(8)
-        Pin(18, Pin.IN)
-        Pin(19, Pin.OUT).value(i % 2)
+
+        #enabled = ~ enabled
+        #set_gpio_out(enabled, mask)
+        set_gpio_out(enabled & seg1, mask)
+
+        Pin(32, Pin.IN)
+        Pin(33, Pin.OUT).value(1)
+        await uasyncio.sleep_ms(8)
+
+        enabled = ~ enabled
+        set_gpio_out(enabled, mask)
+
+        Pin(32, Pin.OUT).value(0)
+        Pin(33, Pin.IN)
+        await uasyncio.sleep_ms(8)
+
+        #enabled = ~ enabled
+        #set_gpio_out(enabled, mask)
+        set_gpio_out(enabled & seg2, mask)
+
+        Pin(32, Pin.IN)
+        Pin(33, Pin.OUT).value(0)
         await uasyncio.sleep_ms(8)
 
 def run():
     loop = uasyncio.get_event_loop()
     loop.create_task(runCommon())
+    #loop.create_task(runSegment())
     loop.run_forever()
