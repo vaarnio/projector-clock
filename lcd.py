9b848de8d1cfdf0b37a60d216d5f97515c30eff8
Valtteri <github@vaarn.io>
2022-01-31 11:32:40 +0200

    First digit can now be set to 1 or 2 (first digit in a clock can only be these two values)

--- lcd.py
+++ lcd.py
@@ -7,28 +7,6 @@ def set_gpio_out(value:int, mask:int):
     GPIO_OUT = ptr32(0x3FF44004) # GPIO Output register
     GPIO_OUT[0] = (GPIO_OUT[0] & mask) | value
 
-@micropython.viper
-def set_gpio_in(value:int, mask:int):
-    GPIO_FUNC_18_OUT_SEL_CFG = ptr32(0x3FF44530+0x4*18)
-    GPIO_FUNC_19_OUT_SEL_CFG = ptr32(0x3FF44530+0x4*19)
-    GPIO_ENABLE = ptr32(0x3FF44020)
-    IO_MUX_18 = ptr32(0x3FF49070)
-    IO_MUX_19 = ptr32(0x3FF49074)
-
-    GPIO_FUNC_18_OUT_SEL_CFG[10] = 1
-    GPIO_FUNC_19_OUT_SEL_CFG[10] = 0
-    GPIO_ENABLE[18] = 1
-    GPIO_ENABLE[19] = 0
-    IO_MUX_18[13] = 1 #MCU_SEL to 0b010
-    IO_MUX_19[13] = 1
-    IO_MUX_18[9] = 0  #FUN_IE
-    IO_MUX_19[9] = 1
-    IO_MUX_18[8] = 1  #pullup
-    IO_MUX_19[8] = 0
-    IO_MUX_18[7] = 0  #pulldown disable
-    IO_MUX_19[7] = 0
-
-
 def initPins():
     Pin(32, Pin.OUT).value(0)
     Pin(33, Pin.OUT).value(0)
@@ -48,47 +26,70 @@ def initPins():
     Pin(25, Pin.OUT).value(0)
     Pin(26, Pin.OUT).value(0)
     Pin(27, Pin.OUT).value(0)
-    
+
+com1 = 0b11111111111111111111111111111111
+com2 = 0b11111111111111111111111111111111
+
+def com1Digit(digit, value):
+    global com1
+    digits = [
+        { #digit 0
+            1: 0b00001000000000000000000000000000,
+            2: 0b00000000000000000000000000000000
+        },
+    ]
+    com1 = com1 ^ digits[digit][value]
+
+def com2Digit(digit, value):
+    global com2
+    digits = [
+        { #digit 0
+            1: 0b00000100000000000000000000000000,
+            2: 0b00001100000000000000000000000000
+        },
+    ]
+    com2 = com2 ^ digits[digit][value]
+
+def setDigit(digit, value):
+    com1Digit(digit, value)
+    com2Digit(digit, value)
+
 async def runCommon():
     print("com task started")
 
     initPins()
-    
-    seg1    = 0b11111111111111110111111111111111
-    seg2    = 0b11111111111111111111111111111111
+    global com1
+    global com2
+    setDigit(0, 2)
+
     enabled = 0b11111111111111111111111111111111
-    #mask =    0b11111111111111110111111111111111
-    mask    = 0b11111111111111110111111111111111 
+    mask    = 0b11110011111111100111111111111111 
 
     i = 0
     while(True):
         i += 1
 
         enabled = ~ enabled
-        set_gpio_out(enabled, mask)
+        set_gpio_out(enabled | com1, mask)
 
         Pin(32, Pin.OUT).value(1)
         Pin(33, Pin.IN)
         await uasyncio.sleep_ms(8)
 
-        #enabled = ~ enabled
-        #set_gpio_out(enabled, mask)
-        set_gpio_out(enabled & seg1, mask)
+        set_gpio_out(enabled | com2, mask)
 
         Pin(32, Pin.IN)
         Pin(33, Pin.OUT).value(1)
         await uasyncio.sleep_ms(8)
 
         enabled = ~ enabled
-        set_gpio_out(enabled, mask)
+        set_gpio_out(enabled ^ com1, mask)
 
         Pin(32, Pin.OUT).value(0)
         Pin(33, Pin.IN)
         await uasyncio.sleep_ms(8)
 
-        #enabled = ~ enabled
-        #set_gpio_out(enabled, mask)
-        set_gpio_out(enabled & seg2, mask)
+        set_gpio_out(enabled ^ com2, mask)
 
         Pin(32, Pin.IN)
         Pin(33, Pin.OUT).value(0)
