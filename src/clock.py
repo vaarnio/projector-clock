281fa9891de28c87706f5e2797d80fd6d3f1ad2e
Valtteri <github@vaarn.io>
2022-02-01 00:31:43 +0200

    Correct timezone is now automatically set

--- src/clock.py
+++ src/clock.py
@@ -1,5 +1,8 @@
 import lcd
+import socket
 import network
+import ntptime
+import time
 
 def do_connect():
     wifiFile = open('./secret/wifi', 'r')
@@ -13,6 +16,56 @@ def do_connect():
             pass
     print('network config:', sta_if.ifconfig())
 
+TIMEZONE_DIFFERENCE = 1
+dst_on = False
+
+def resolve_dst_and_set_time():
+    global TIMEZONE_DIFFERENCE
+    global dst_on
+    # This is most stupid thing what humans can do!
+    # Rules for Finland: DST ON: March last Sunday at 03:00 + 1h, DST OFF: October last Sunday at 04:00 - 1h
+    # Sets localtime to DST localtime
+    if network.WLAN(network.STA_IF).config('essid') == '':
+        now = time.mktime(time.localtime())
+        if DEBUG_ENABLED == 1:
+            print("Network down! Can not set time from NTP!")
+    else:
+        now = ntptime.time()
+
+    (year, month, mdate, hour, minute, second, wday, yday) = time.localtime(now)
+
+    if year < 2020:
+        if DEBUG_ENABLED == 1:
+            print("Time not set correctly!")
+        return False
+
+    dstend = time.mktime((year, 10, (31 - (int(5 * year / 4 + 1)) % 7), 4, 0, 0, 0, 6, 0))
+    dstbegin = time.mktime((year, 3, (31 - (int(5 * year / 4 + 4)) % 7), 3, 0, 0, 0, 6, 0))
+
+    if TIMEZONE_DIFFERENCE >= 0:
+        if (now > dstbegin) and (now < dstend):
+            dst_on = True
+            ntptime.NTP_DELTA = 3155673600 - ((TIMEZONE_DIFFERENCE + 1) * 3600)
+        else:
+            dst_on = False
+            ntptime.NTP_DELTA = 3155673600 - (TIMEZONE_DIFFERENCE * 3600)
+    else:
+        if (now > dstend) and (now < dstbegin):
+            dst_on = False
+            ntptime.NTP_DELTA = 3155673600 - (TIMEZONE_DIFFERENCE * 3600)
+        else:
+            dst_on = True
+            ntptime.NTP_DELTA = 3155673600 - ((TIMEZONE_DIFFERENCE + 1) * 3600)
+    if dst_on is None:
+        if DEBUG_ENABLED == 1:
+            print("DST calculation failed!")
+            return False
+    else:
+        ntptime.settime()
+
 def run():
     do_connect()
+    resolve_dst_and_set_time()
+    print(time.localtime())
+    #print(get_time())
     lcd.run()
