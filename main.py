fd96aa9a45edb60db31b30e68cd87d06830fe3bd
Valtteri <github@vaarn.io>
2022-01-31 22:17:02 +0200

    added default wifi connect script

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import lcd
+import network
 
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('ssid', 'pass')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+do_connect()
 lcd.run()
